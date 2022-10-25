787bed4032f40a2fb9d0159f01c0ac916ca77082
HeyArHei <61173315+heihei0314@users.noreply.github.com>
2022-10-25 15:59:35 +0800

    Delete db_configs.php

--- referee system/poomsae/db_configs.php
+++
@@ -1,7 +0,0 @@
-<?php
-define("host","45.113.121.87", false);
-define("username","waits3ql_tkd", false);
-define("password","Al245935", false);
-define("dbname","waits3ql_poomsae", false);
-define("refereePW","tkdreferee", false);
-?>
